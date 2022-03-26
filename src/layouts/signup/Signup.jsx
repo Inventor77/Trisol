3f6631772f9b34fa9ef7b030052de24bd715ecc0
Shreedutt <shreedutt77@gmail.com>
2022-03-26 13:55:59 +0530

    Signup contaier Right is developed and styled

--- src/layouts/signup/Signup.jsx
+++ src/layouts/signup/Signup.jsx
@@ -1,11 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.scss'
 import Logo from '../../assets/svgs/logo.svg'
+import EyeOpen from '../../assets/svgs/eyeOpen.svg'
+import EyeClosed from '../../assets/svgs/eyeClosed.svg'
 import InfoHeader from '../../components/Info/InfoHeader'
 import InfoPara from '../../components/Info/InfoPara'
 import InfoCheckStyle from '../../components/Info/InfoCheckStyle'
+import InputUnstyled from '@mui/base/InputUnstyled';
+import { styled } from '@mui/system';
+import {
+    FormControl,
+    IconButton,
+    Select,
+    OutlinedInput,
+    MenuItem,
+    Button
+} from '@mui/material';
+
+const StyledInputElement = styled('input')(
+    ({ theme }) => `
+    width: 372px;
+    font-size: 0.875rem;
+    font-family: "Roboto", serif;
+    font-weight: 400;
+    line-height: 1.5;
+    color:  #1A2027;
+    background: #F3F6F9;
+    border: 1px solid #CDD2D7;
+    border-radius: 5px;
+    padding: 12px;
+
+    &:hover {
+        background: #E7EBF0;
+        border-color: #B2BAC2;
+    }
+
+    &:focus {
+        outline: 3px solid #0288d1;
+    }
+`,
+);
+
+
+const CustomInput = React.forwardRef(function CustomInput(props, ref) {
+    return (
+        <InputUnstyled components={{ Input: StyledInputElement }} {...props} ref={ref} />
+    );
+});
+
+const StyledSelectElement = styled(Select)(
+    ({ theme }) => `
+    width: 372px;
+    height: 48px;
+    font-size: 0.875rem;
+    font-family: "Roboto", serif;
+    font-weight: 500;
+    line-height: 1.5;
+    color:  #1A2027;
+    background: #F3F6F9;
+    border: 1px solid #CDD2D7;
+    border-radius: 4px;
+
+    &:hover {
+        background: #E7EBF0;
+        border-color: #B2BAC2;
+    }
+
+    &:focus {
+        outline: 3px solid #0288d1;
+    }
+`,
+);
+
+
+const StyledButton = styled(Button)(() => `
+    height: 49px;
+    width: 372px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 20px;
+    background: #777777;
+    border-radius: 4px;
+    text-align: left;
+    margin-top: 42px;
+`)
+
+const DESCRIPTION = [
+    "Doctor",
+    "Care Seeker",
+    'Radiologist'
+]
 
 function Signup() {
+    const [visible, setVisible] = useState(false);
+    const [description, setDescription] = useState("");
+
+    const handleChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setDescription(value);
+    };
+
     return (
         <div className='signup_layout'>
             <div className='signup_container'>
@@ -16,6 +114,66 @@ function Signup() {
                     <InfoHeader />
                     <InfoPara />
                     <InfoCheckStyle />
+                </div>
+                <div className='signup_container_right'>
+                    <div className='signup_container_right_title'>
+                        <h1 className='title_text'>
+                            Start with Trisol
+                        </h1>
+                    </div>
+                    <div className='signup_container_right_para'>
+                        <p className='para_text'>
+                            Continue with Logging in with your details.
+                        </p>
+                    </div>
+                    <FormControl>
+                        <label htmlFor="name" className='form_label'>NAME</label>
+                        <CustomInput id="name" aria-label="Name" placeholder="Please Insert your full legal name" />
+                        <label htmlFor="password" className='form_label'>PASSWORD</label>
+                        <div className='password_container'>
+                            <CustomInput
+                                id="password"
+                                aria-label="Password"
+                                placeholder="Password"
+                                type={visible ? "" : "password"}
+                            />
+                            <IconButton className='password_eye' onClick={() => setVisible(!visible)}>
+                                <img src={visible ? EyeOpen : EyeClosed} alt='eye'></img>
+                            </IconButton>
+                        </div>
+                        <label htmlFor="description" className='form_label'>DESCRIPTION</label>
+                        <StyledSelectElement
+                            displayEmpty
+                            value={description}
+                            onChange={handleChange}
+                            input={<OutlinedInput />}
+                            renderValue={(selected) => {
+                                if (selected.length === 0) {
+                                    return <em>Please Choose</em>;
+                                }
+                                return selected;
+                            }}
+                        >
+                            <MenuItem disabled value="">
+                                <em>Please Choose</em>
+                            </MenuItem>
+                            {DESCRIPTION.map((name) => (
+                                <MenuItem
+                                    key={name}
+                                    value={name}
+                                >
+                                    {name}
+                                </MenuItem>
+                            ))}
+                        </StyledSelectElement>
+                        <StyledButton variant="contained">Sign up</StyledButton>
+                    </FormControl>
+                    <div className='signup_container_right_footer'>
+                        <h1 className='footer_text'>
+                            Already have an Account? 
+                        </h1>
+                        <span className='footer_link'>Log In</span>
+                    </div>
                 </div>
             </div>
         </div>
